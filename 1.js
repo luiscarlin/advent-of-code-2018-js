9d7b8d31697772333dd072d3e52b4a8ac7a5de66
Luis Carlin <luiscarlin@gmail.com>
2019-02-08 06:28:18 -0500

    put into a funcetion ready for testing

--- 1.js
+++ 1.js
@@ -2,8 +2,13 @@ const fs = require('fs')
 
 let lines = fs.readFileSync('./1.in', 'utf8').split('\n')
 
-const total = lines.filter(line => line)
-  .map(num => parseInt(num))
-  .reduce((total, num) => total + num)
+console.log('part 1', findTotal(lines))
 
-console.log('part 1', total)
+function findTotal(lines) {
+  return lines
+    .filter(line => line)
+    .map(num => parseInt(num))
+    .reduce((total, num) => total + num)
+}
+
+export { findTotal }
