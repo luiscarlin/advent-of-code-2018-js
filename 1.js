bc174b00bffdf8c522a1f4a39daaeb2ef6f1ffa1
Luis Carlin <luiscarlin@gmail.com>
2019-02-08 06:22:24 -0500

    refactor so good

--- 1.js
+++ 1.js
@@ -2,12 +2,8 @@ const fs = require('fs')
 
 let lines = fs.readFileSync('./1.in', 'utf8').split('\n')
 
-let total = 0
-
-lines.forEach(num => {
-  if (num){
-    total += parseInt(num)
-  }
-})
+const total = lines.filter(line => line)
+  .map(num => parseInt(num))
+  .reduce((total, num) => total + num)
 
 console.log('part 1', total)
