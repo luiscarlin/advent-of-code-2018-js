baae438e348d1d6a3d698620245b43cb5ca2d7f2
Luis Carlin <luiscarlin@gmail.com>
2019-02-23 10:29:16 -0500

    clean up

--- 1.test.js
+++ 1.test.js
@@ -1,20 +1,19 @@
-const one = require('./1')
-// const getFirstRepeatedFreq = require('./1').getFirstRepeatedFreq
+const subject = require('./1')
 
 describe('day1', () => {
   describe('sumList', () => {
     it('calculates total sum of numbers in array with positive numbers and zeros', () => {
-      expect(one.sumList([3, 1, 2, 0])).toBe(6)
+      expect(subject.sumList([3, 1, 2, 0])).toBe(6)
     })
 
     it('calculates total sum of numbers in array with negative numbers', () => {
-      expect(one.sumList([1, 5, -6, 0])).toBe(0)
+      expect(subject.sumList([1, 5, -6, 0])).toBe(0)
     })
   })
 
   describe('getFirstRepeatedFreq', () => {
     it('retruns first repeated frequency', () => {
-      expect(one.getFirstRepeatedFreq([2, 3, -3])).toBe(2)
+      expect(subject.getFirstRepeatedFreq([2, 3, -3])).toBe(2)
     })
   })
 })
