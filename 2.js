4b4deb8f4d526fc6a2bad42edf0803041e64aa24
Luis Carlin <luiscarlin@gmail.com>
2019-02-23 11:29:12 -0500

    get count based on repeated parameter

--- 2.js
+++ 2.js
@@ -1,8 +1,8 @@
-function getCountRepeated(str, repated) {
+function getCountRepeated(str, repeated) {
   let count = {}
-  str.split('').forEach(letter => count[letter] = letter in count ? count[letter] + 1 : 0)
+  str.split('').forEach(letter => count[letter] = letter in count ? count[letter] + 1 : 1)
 
-  return Object.values(count).reduce((accumulator, current) => current == repated ? accumulator += 1: accumulator)
+  return Object.values(count).reduce((accumulator, current) => (current == repeated ? accumulator + 1: accumulator), initalValue = 0)
 }
 
 module.exports = { getCountRepeated }
--- 2.test.js
+++ 2.test.js
@@ -6,9 +6,20 @@ describe('day2', () => {
       const actual = subject.getCountRepeated('asda', 2)
       expect(actual).toBe(1)
     })
+
     it('returns 0 if there are NO letters that repeat twice and requesting repeated twice', () => {
       const actual = subject.getCountRepeated('asdmnb', 2)
       expect(actual).toBe(0)
+    })
+
+    it('returns 1 when letter is repeated 3 times and requesting repeated 3 times', () => {
+      const actual = subject.getCountRepeated('asdaa', 3)
+      expect(actual).toBe(1)
+    })
+
+    it('returns 2 when 2 letters repeat 3 times and requesting repeated 3 times', () => {
+      const actual = subject.getCountRepeated('asdxaaxx', 3)
+      expect(actual).toBe(2)
     })
   })
 })
