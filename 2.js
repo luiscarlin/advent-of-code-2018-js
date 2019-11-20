180dc2e22a6d2285bd8f2c4a5ff2f5b5c60cb38f
Luis Carlin <luiscarlin@gmail.com>
2019-11-19 21:34:44 -0500

    part 2

--- 2.js
+++ 2.js
@@ -1,6 +1,22 @@
 const fs = require('fs');
 
-const main = () => {
+const isOffByOne = (line1, line2) => {
+  let offBy = 0;
+  let same = [];
+
+  for (let i = 0; i < line1.length; i++) {
+    if (line1[i] != line2[i]) offBy++;
+    else {
+      same.push(line1[i]);
+    }
+  }
+
+  return offBy === 1
+    ? { offByOne: true, same: same.join('') }
+    : { offByOne: false, same: same.join('') };
+};
+
+const part1 = () => {
   let lines = fs.readFileSync('./2.in', 'utf8').split('\n');
 
   let numberDouble = 0;
@@ -22,6 +38,26 @@ const main = () => {
   });
 
   console.log('part 1', numberDouble * numberTriple);
+};
+
+const part2 = () => {
+  let lines = fs.readFileSync('./2.in', 'utf8').split('\n');
+
+  lines.forEach(line => {
+    lines.forEach(otherLine => {
+      const { offByOne, same } = isOffByOne(line, otherLine);
+
+      if (offByOne) {
+        console.log('part2', same);
+        process.exit();
+      }
+    });
+  });
+};
+
+const main = () => {
+  part1();
+  part2();
 };
 
 if (require.main == module) {
