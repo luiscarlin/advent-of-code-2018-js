609d7f1e4e40e35c71eca508cf9cc0e5d42d85eb
Luis Carlin <luiscarlin@gmail.com>
2019-11-19 21:11:27 -0500

    add prettier and finish part 1

--- 2.js
+++ 2.js
@@ -1,8 +1,29 @@
-function getCountRepeated(str, repeated) {
-  let count = {}
-  str.split('').forEach(letter => count[letter] = letter in count ? count[letter] + 1 : 1)
+const fs = require('fs');
 
-  return Object.values(count).reduce((accumulator, current) => (current == repeated ? accumulator + 1: accumulator), initalValue = 0)
+const main = () => {
+  let lines = fs.readFileSync('./2.in', 'utf8').split('\n');
+
+  let numberDouble = 0;
+  let numberTriple = 0;
+
+  lines.forEach(line => {
+    const count = {};
+
+    line
+      .split('')
+      .forEach(
+        letter => (count[letter] = count[letter] ? count[letter] + 1 : 1)
+      );
+
+    const values = Object.values(count);
+
+    if (values.includes(2)) numberDouble++;
+    if (values.includes(3)) numberTriple++;
+  });
+
+  console.log('part 1', numberDouble * numberTriple);
+};
+
+if (require.main == module) {
+  main();
 }
-
-module.exports = { getCountRepeated }
