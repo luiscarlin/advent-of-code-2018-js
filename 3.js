bacf4c09f45f471fd80cb6d5abfeef31d885bb56
Luis Carlin <luiscarlin@gmail.com>
2019-11-21 21:47:40 -0500

    finish part1

--- 3.js
+++ 3.js
@@ -1,9 +1,45 @@
 const fs = require('fs');
 
-const main = () => {
-  let lines = fs.readFileSync('./2.in', 'utf8').split('\n');
+const parseClaim = claim => {
+  let parsedClaim = claim.split(' ');
 
-  console.log(lines);
+  let id = parsedClaim[0];
+
+  let [x, y] = parsedClaim[2].split(',');
+  x = Number(x);
+  y = Number(y.slice(0, -1));
+
+  let [width, height] = parsedClaim[3].split('x');
+  width = Number(width);
+  height = Number(height);
+
+  return { id, x, y, width, height };
+};
+
+const main = () => {
+  let lines = fs.readFileSync('./3.in', 'utf8').split('\n');
+
+  const coordinates = {};
+
+  for (let line of lines) {
+    const { id, x, y, width, height } = parseClaim(line);
+
+    for (let dx of Array.from(Array(width).keys())) {
+      for (let dy of Array.from(Array(height).keys())) {
+        coordinates[[x + dx, y + dy]] = coordinates[[x + dx, y + dy]]
+          ? coordinates[[x + dx, y + dy]] + 1
+          : 1;
+      }
+    }
+  }
+
+  let sharedSquares = 0;
+
+  for (let count of Object.values(coordinates)) {
+    if (count > 1) sharedSquares++;
+  }
+
+  console.log('part 1', sharedSquares);
 };
 
 if (require.main == module) {
